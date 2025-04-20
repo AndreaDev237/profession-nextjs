4bfa005d5376578d45736e817ee4a4498471c381
andreadev237 <andreadev237@gmail.com>
2025-04-20 15:14:07 +0200

    Modified page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
-          <li>SONO UN NUOVO ELEMENTO</li>
+          <li>MODIFICA ELEMENTO</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
