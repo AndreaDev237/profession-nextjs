ad9ad03a87c7600ba6a0e4ca9f66dae847ec5b96
andreadev237 <andreadev237@gmail.com>
2025-04-21 10:56:54 +0200

    Added text for Netlify Deploy

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
-          <li>MODIFICA ELEMENTO PER NETLIFY</li>
+          <li>MODIFICA ELEMENTO PER NETLIFY PARTE 2</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
