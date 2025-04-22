57732e8c32d75674943ef9f951abdd28d5916173
andreadev237 <andreadev237@gmail.com>
2025-04-22 15:57:58 +0200

    Developer Preview of form submission

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,20 @@ export default function Home() {
           <li>MODIFICA ELEMENTO PER NETLIFY PARTE 2</li>
         </ol>
 
+        <div>
+        <form name="contact" netlify>
+  <p>
+    <label>Name <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Email <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+        </div>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
